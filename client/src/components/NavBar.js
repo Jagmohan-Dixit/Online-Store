e4dd4a2ca55c323d8ea414ef1df76c4b0e43929b
Peter Sviryd <Peter.blr@yandex.ru>
2021-10-05 23:38:33 +0300

    add registration panel

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
-import {Button, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import {SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
@@ -10,17 +10,19 @@ const NavBar = observer(() => {
 
     return (
         <Navbar bg="dark" variant="dark">
+            <Container>
             <NavLink style={{color: 'red'}} to={SHOP_ROUTE}>StudyТехноПлюс</NavLink>
             {user.isAuth ?
-                <Nav className="m-lg-auto" style={{color: 'white'}}>
-                    <Button variant={"outline-light"}>Админ панель</Button>
-                    <Button variant={"outline-light"} className="m-lg-4">Выйти</Button>
+                <Nav className="ml-auto" style={{color: 'white'}}>
+                    <Button variant={"outline-light"} className="m-2">Админ панель</Button>
+                    <Button variant={"outline-light"} className="m-2">Выйти</Button>
                 </Nav>
                 :
-                <Nav className="m-lg-auto" style={{color: 'white'}}>
+                <Nav className="ml-auto" style={{color: 'white'}}>
                     <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
                 </Nav>
             }
+            </Container>
         </Navbar>
     );
 });
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import {Button, Card, Container, Form} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(location)
     return (
-        <div>
-            AUTH
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите email"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите password"
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ?
+                            <div>
+                                Хотите создать аккаунт? <NavLink to={REGISTRATION_ROUTE}>Ходь сюды!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Аккаунт не забыли? <NavLink to={LOGIN_ROUTE}>Тады сюды!</NavLink>
+                            </div>
+                        }
+                        <Button
+                            className="align-self-end"
+                            variant={"outline-danger"}
+                        >
+                            {isLogin ? 'Войти' : 'Регистрация'}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+
+        </Container>
     );
 };
 
