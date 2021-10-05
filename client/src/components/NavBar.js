43d69b93f64726fed0cfad5dd934997a6bfa527e
Peter Sviryd <Peter.blr@yandex.ru>
2021-10-05 15:10:44 +0300

    start create NavBar next part

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,28 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
+import {Button, Nav, Navbar} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import {SHOP_ROUTE} from "../utils/consts";
+import {observer} from "mobx-react-lite";
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context)
 
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <NavLink style={{color: 'red'}} to={SHOP_ROUTE}>StudyТехноПлюс</NavLink>
+            {user.isAuth ?
+                <Nav className="m-lg-auto" style={{color: 'white'}}>
+                    <Button variant={"outline-light"}>Админ панель</Button>
+                    <Button variant={"outline-light"} className="m-lg-4">Выйти</Button>
+                </Nav>
+                :
+                <Nav className="m-lg-auto" style={{color: 'white'}}>
+                    <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                </Nav>
+            }
+        </Navbar>
     );
-};
+});
 
 export default NavBar;
